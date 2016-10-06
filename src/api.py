a0dce097c82f2fc50921eee1aeeb3e3199d8c3ff
Sami Jawhar <sami.n.jawhar@gmail.com>
2016-10-06 08:45:31 -0700

    Added handling of translator errors in api

--- src/api.py
+++ src/api.py
@@ -7,15 +7,26 @@ class PigLatinProcedure(object):
         try:
             doc = request.context['doc']
             text = doc['text']
+            
+            translator = PigLatinTranslator()
+            translation = translator.convert(text)
         except KeyError:
             raise falcon.HTTPBadRequest(
                 'No text',
                 'The request body must contain text to translate.'
             )
+        except Exception,e:
+            description = None
+            if (len(e.args) > 0):
+                description = e.args[0]
 
-        translator = PigLatinTranslator()
+            raise falcon.HTTPInternalServerError(
+                'Server error',
+                description
+            )
+
         response.status = falcon.HTTP_200
-        request.context['result'] = {'translation': translator.convert(text)}
+        request.context['result'] = {'translation': translation}
 
 procedure = PigLatinProcedure()
 app = falcon.API(middleware=[
