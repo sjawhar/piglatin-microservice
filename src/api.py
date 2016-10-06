adec8ac6acfca74bdd75e569c552ba22ba7daee0
Sami Jawhar <sami.n.jawhar@gmail.com>
2016-10-06 09:02:56 -0700

    Refactored error handling in API

--- src/api.py
+++ src/api.py
@@ -4,24 +4,33 @@ from piglatin import PigLatinTranslator
 
 class PigLatinProcedure(object):
     def on_post(self, request, response):
-        try:
-            doc = request.context['doc']
-            text = doc['text']
-            
-            translator = PigLatinTranslator()
-            translation = translator.convert(text)
-        except KeyError:
+        if request.content_length in (None, 0):
             raise falcon.HTTPBadRequest(
                 'No text',
                 'The request body must contain text to translate.'
             )
+
+        doc = request.context['doc']
+        if 'text' not in doc:
+            raise falcon.HTTPMissingParam('text')
+        elif len(doc['text']) == 0:
+            raise falcon.HTTPInvalidParam(
+                'The value is empty.',
+                'text'
+            )
+        
+        text = doc['text']
+        
+        try:
+            translator = PigLatinTranslator()
+            translation = translator.convert(text)
         except Exception,e:
             description = None
             if (len(e.args) > 0):
                 description = e.args[0]
 
             raise falcon.HTTPInternalServerError(
-                'Server error',
+                'Unexpected error',
                 description
             )
 
