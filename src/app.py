11b9056826e31274e711f4681d3574cb39caf738
Sami Jawhar <sami.n.jawhar@gmail.com>
2016-10-06 17:23:52 -0700

    Added documentation to app.py

--- src/app.py
+++ src/app.py
@@ -29,6 +29,9 @@ class PigLatinProcedure(object):
         try:
             translation = piglatin.to_pig_latin(text)
         except Exception,e:
+            """API-specific input validation was handled before calling piglatin.to_pig_latin().
+            So any errors are unexpected and given generic 500 code.
+            Error message is retained, if possible"""
             description = None
             if (len(e.args) > 0):
                 description = e.args[0]
